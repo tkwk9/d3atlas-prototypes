4ec4f07c69f886e5e5a1cbc69a2654f5856d913d
tkwk9 <tim.k.kwak@gmail.com>
2023-10-04 18:35:30 -0700

    update zonestack management

--- src/pages/UsTwo.js
+++ src/pages/UsTwo.js
@@ -23,8 +23,15 @@ const layerToSelectableZone = {
 };
 
 const getIsSelectable = (state, zoneType) =>
-  layerToSelectableZone[state.currentLayer] === zoneType;
+  layerToSelectableZone[
+    state.zoneStack[state.zoneStack.length - 1].zoneType
+  ] === zoneType;
+const getFocusId = (state) =>
+  state.zoneStack[state.zoneStack.length - 1].zoneId;
+const getCurrentZoneViewBox = (state) =>
+  state.zoneStack[state.zoneStack.length - 1].zoneViewBox;
 
+// Data Fetcher
 const getFips = async () => {
   const data = await csv(countyFipsCsvPath);
   const countyGeometries = UsTopo.objects.counties.geometries.reduce(
@@ -70,21 +77,20 @@ const getFips = async () => {
   }, {});
 };
 
+// Main Component
 const UsTwo = (props) => {
   onMount(() => console.log(document.getElementById("MainSVG")));
   const [state, setState] = createStore({
-    viewBox: null,
-    prevFocusId: null,
-    focusId: null,
     zoneStack: [
       {
         zoneId: "US",
         zoneType: "nation",
         zoneName: "United States",
         zoneAbbr: "US",
+        // original box: 0 0 975 610
+        zoneViewBox: "-40 -40 1055 690",
       },
     ],
-    currentLayer: "nation",
   });
 
   const path = geoPath();
@@ -92,22 +98,19 @@ const UsTwo = (props) => {
   const handleZoneClick = (e) => {
     setState((prevState) => {
       const bBox = e.target.getBBox();
-      const viewBox = `${bBox.x - 40} ${bBox.y - 40} ${bBox.width + 80} ${
+      const zoneViewBox = `${bBox.x - 40} ${bBox.y - 40} ${bBox.width + 80} ${
         bBox.height + 80
       }`;
-      const prevFocusId = prevState.focusId;
       const { zoneId, zoneType, zoneName, zoneAbbr } = e.target.dataset;
       const zoneStack = [
         ...prevState.zoneStack,
-        { zoneId, zoneType, zoneName, zoneAbbr },
+        { zoneId, zoneType, zoneName, zoneAbbr, zoneViewBox },
       ];
-      const focusId = e.target.dataset.zoneId;
-      const currentLayer = e.target.dataset.zoneType;
-      return { viewBox, prevFocusId, focusId, currentLayer, zoneStack };
+      return { zoneStack };
     });
     anime({
       targets: "#MainSVG",
-      viewBox: state.viewBox,
+      viewBox: getCurrentZoneViewBox(state),
       duration: 500,
       easing: "easeInQuad",
       complete: () => console.log("done"),
@@ -115,49 +118,48 @@ const UsTwo = (props) => {
   };
 
   return (
-      <svg
-        class="UsTwo"
-        id="MainSVG"
-        width="100%"
-        height="100%"
-        // original box: 0 0 975 610
-        viewBox={`-40 -40 1055 690`}
-      >
-        {Object.entries(props.fips).map(([stateId, v]) => (
-          <g id={`g${stateId}`} class="UsTwo-stateGroup">
-            <path
-              id={stateId}
-              class={`UsTwo-state UsTwo-zone`}
-              data-zone-type="state"
-              data-zone-id={stateId}
-              data-zone-name={v.name}
-              data-zone-abbr={v.abbr}
-              onclick={handleZoneClick}
-              d={path(topojson.feature(UsTopo, v.geometry))}
-              stroke="#aaa"
-              stroke-width="0.5"
-              fill={getIsSelectable(state, "state") ? "#636b78" : "None"}
-            />
-            {Object.entries(v.counties).map(([countyId, v]) => (
-              <g id={`g${countyId}`} class="UsTwo-countyGroup">
-                <path
-                  id={countyId}
-                  class={`UsTwo-county UsTwo-zone`}
-                  data-zone-type="county"
-                  data-zone-id={countyId}
-                  data-zone-name={v.name}
-                  data-zone-abbr={v.abbr}
-                  onclick={handleZoneClick}
-                  d={path(topojson.feature(UsTopo, v.geometry))}
-                  stroke="#aaa"
-                  stroke-width="0.1"
-                  fill={getIsSelectable(state, "county") ? "#636b78" : "None"}
-                />
-              </g>
-            ))}
-          </g>
-        ))}
-      </svg>
+    <svg
+      class="UsTwo"
+      id="MainSVG"
+      width="100%"
+      height="100%"
+      viewBox={`-40 -40 1055 690`}
+    >
+      {Object.entries(props.fips).map(([stateId, v]) => (
+        <g id={`g${stateId}`} class="UsTwo-stateGroup">
+          <path
+            id={stateId}
+            class={`UsTwo-state UsTwo-zone`}
+            data-zone-type="state"
+            data-zone-id={stateId}
+            data-zone-name={v.name}
+            data-zone-abbr={v.abbr}
+            onclick={handleZoneClick}
+            d={path(topojson.feature(UsTopo, v.geometry))}
+            stroke="#aaa"
+            stroke-width="0.5"
+            fill={getIsSelectable(state, "state") ? "#636b78" : "None"}
+          />
+          {Object.entries(v.counties).map(([countyId, v]) => (
+            <g id={`g${countyId}`} class="UsTwo-countyGroup">
+              <path
+                id={countyId}
+                class={`UsTwo-county UsTwo-zone`}
+                data-zone-type="county"
+                data-zone-id={countyId}
+                data-zone-name={v.name}
+                data-zone-abbr={v.abbr}
+                onclick={handleZoneClick}
+                d={path(topojson.feature(UsTopo, v.geometry))}
+                stroke="#aaa"
+                stroke-width="0.1"
+                fill={getIsSelectable(state, "county") ? "#636b78" : "None"}
+              />
+            </g>
+          ))}
+        </g>
+      ))}
+    </svg>
   );
 };
 
@@ -165,7 +167,7 @@ export default () => {
   const [fips] = createResource(getFips);
   return (
     <Show when={fips()}>
-      <UsTwo fips={fips()}/>
+      <UsTwo fips={fips()} />
     </Show>
-  )
-}
+  );
+};
