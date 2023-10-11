b4d57988b8351cdd5333200fe7874f793c339db1
tkwk9 <tim.k.kwak@gmail.com>
2023-10-10 19:19:01 -0700

    create box

--- src/pages/Comparator.js
+++ src/pages/Comparator.js
@@ -3,7 +3,14 @@ import { Show, createResource } from "solid-js";
 import "./Comparator.scss";
 
 const Comparator = () => {
-    return <div>Comparator</div>
-}
+  return (
+    <div class="Comparator">
+      <h1 class="title">Comparator: GoFCSSPJS</h1>
+      <div class="wrapper box">
+        <div class="content box" />
+      </div>
+    </div>
+  );
+};
 
 export default Comparator;
