73bcfb838415fe2f6526ddfb84647c964cc5f759
tkwk9 <tim.k.kwak@gmail.com>
2023-10-14 18:13:51 -0700

    bruteforce more

--- src/pages/Minesweeper.js
+++ src/pages/Minesweeper.js
@@ -1,21 +1,103 @@
-import { onMount, createSignal } from "solid-js";
+import { createStore } from "solid-js/store";
 import anime from "animejs";
 import Victor from "victor";
 
 import "./Minesweeper.scss";
 
 const Minesweeper = () => {
-  const map = [
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+  const tmpMap = [
+    [null, null, "M", null, null, null, null, null, null],
+    [null, null, null, null, null, null, "M", null, null],
+    [null, null, null, null, null, "M", null, null, null],
+    [null, null, null, null, null, "M", null, null, null],
+    [null, "M", null, null, null, null, null, null, null],
+    ["M", null, null, null, null, null, null, null, "M"],
+    [null, null, null, null, null, null, null, null, 1],
+    [null, null, null, null, 1, null, null, null, null],
+    [null, null, null, null, null, 1, null, null, null],
   ];
+
+  const map = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  const [gameState, setGameState] = createStore({
+    gameState: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  });
+
+  tmpMap.forEach((row, rowIdx) =>
+    row.forEach((cellData, columnIdx) => {
+      if (cellData === "M") {
+        map[rowIdx][columnIdx] = "M";
+        if (
+          map[rowIdx - 1] &&
+          map[rowIdx - 1][columnIdx - 1] !== undefined &&
+          map[rowIdx - 1][columnIdx - 1] !== "M"
+        )
+          map[rowIdx - 1][columnIdx - 1] += 1;
+        if (
+          map[rowIdx - 1] &&
+          map[rowIdx - 1][columnIdx] !== undefined &&
+          map[rowIdx - 1][columnIdx] !== "M"
+        )
+          map[rowIdx - 1][columnIdx] += 1;
+        if (
+          map[rowIdx - 1] &&
+          map[rowIdx - 1][columnIdx + 1] !== undefined &&
+          map[rowIdx - 1][columnIdx + 1] !== "M"
+        )
+          map[rowIdx - 1][columnIdx + 1] += 1;
+        if (
+          map[rowIdx] &&
+          map[rowIdx][columnIdx - 1] !== undefined &&
+          map[rowIdx][columnIdx - 1] !== "M"
+        )
+          map[rowIdx][columnIdx - 1] += 1;
+        if (
+          map[rowIdx] &&
+          map[rowIdx][columnIdx + 1] !== undefined &&
+          map[rowIdx][columnIdx + 1] !== "M"
+        )
+          map[rowIdx][columnIdx + 1] += 1;
+        if (
+          map[rowIdx + 1] &&
+          map[rowIdx + 1][columnIdx - 1] !== undefined &&
+          map[rowIdx + 1][columnIdx - 1] !== "M"
+        )
+          map[rowIdx + 1][columnIdx - 1] += 1;
+        if (
+          map[rowIdx + 1] &&
+          map[rowIdx + 1][columnIdx] !== undefined &&
+          map[rowIdx + 1][columnIdx] !== "M"
+        )
+          map[rowIdx + 1][columnIdx] += 1;
+        if (
+          map[rowIdx + 1] &&
+          map[rowIdx + 1][columnIdx + 1] !== undefined &&
+          map[rowIdx + 1][columnIdx + 1] !== "M"
+        )
+          map[rowIdx + 1][columnIdx + 1] += 1;
+      }
+    })
+  );
 
   return (
     <div class="Minesweeper">
@@ -25,14 +107,11 @@ const Minesweeper = () => {
           return row.map((item) => {
             return (
               <div class={`cell cell-1`}>
-                <span>{item}</span>
+                <span>{item > 0 || item === "M" ? item : ""}</span>
               </div>
             );
           });
         })}
-        {/* <div class={`cell cell-1`} >1</div>
-        <div class={`cell cell-2`} >2</div>
-        <div class={`cell cell-3`} >3</div> */}
       </div>
     </div>
   );
