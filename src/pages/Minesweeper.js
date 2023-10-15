12ce0bc17881f21e41cd51b4bdb8873a2d23e566
tkwk9 <tim.k.kwak@gmail.com>
2023-10-14 17:34:53 -0700

    set up grid

--- src/pages/Minesweeper.js
+++ src/pages/Minesweeper.js
@@ -5,9 +5,35 @@ import Victor from "victor";
 import "./Minesweeper.scss";
 
 const Minesweeper = () => {
+  const map = [
+    [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+  ];
+
   return (
     <div class="Minesweeper">
       <h1 class="title">Minesweeper: F</h1>
+      <div class={`container`}>
+        {map.map((row) => {
+          return row.map((item) => {
+            return (
+              <div class={`cell cell-1`}>
+                <span>{item}</span>
+              </div>
+            );
+          });
+        })}
+        {/* <div class={`cell cell-1`} >1</div>
+        <div class={`cell cell-2`} >2</div>
+        <div class={`cell cell-3`} >3</div> */}
+      </div>
     </div>
   );
 };
