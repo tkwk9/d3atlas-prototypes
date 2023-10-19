5ce9516eff35fbf8523c978b716a287d22aaca20
tkwk9 <tim.k.kwak@gmail.com>
2023-10-18 18:34:29 -0700

    save

--- src/pages/SvgStressTest.js
+++ src/pages/SvgStressTest.js
@@ -8,6 +8,13 @@ const SvgStressTest = () => {
   return (
     <div class="SvgStressTest">
       <h1 class="title">SVG Stress Test: U</h1>
+      <svg
+        id={`MainSvg`}
+        width="100%"
+        height="100%"
+      >
+        <rect x="0" width="100" height="100" rx="15" stroke-width="1" stroke="black" fill="red"/>
+      </svg>
     </div>
   );
 };
