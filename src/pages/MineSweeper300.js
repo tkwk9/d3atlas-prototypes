6b14588a856f144534fbc70ba8d1d22dc656bc2f
tkwk9 <tim.k.kwak@gmail.com>
2023-10-26 20:30:13 -0700

    fix panning

--- src/pages/MineSweeper300.js
+++ src/pages/MineSweeper300.js
@@ -4,7 +4,60 @@ import "./MineSweeper300.scss";
 
 const MineSweeper300 = () => {
   let svgRef;
+
   const [dragPoint, setDragPoint] = createSignal(null);
+
+  const handleMouseDown = (e) => {
+
+    const svgPoint = svgRef.createSVGPoint();
+    svgPoint.x = e.clientX;
+    svgPoint.y = e.clientY;
+    const screenCtm = svgRef.getScreenCTM().inverse();
+    const mappedPoint = svgPoint.matrixTransform( screenCtm );
+
+    setDragPoint({
+      x: svgRef.viewBox.baseVal.x,
+      y: svgRef.viewBox.baseVal.y,
+      width: svgRef.viewBox.baseVal.width,
+      height: svgRef.viewBox.baseVal.height,
+      clientX: e.clientX,
+      clientY: e.clientY,
+      mappedPoint,
+      screenCtm
+    });
+  };
+  const handleMouseUp = (e) => {
+    setDragPoint(null);
+  };
+  const handleMouseLeave = (e) => {
+    setDragPoint(null);
+  };
+
+  const handleWheel = (e) => {
+    svgRef.setAttribute(
+      "viewBox",
+      `${svgRef.viewBox.baseVal.x}, ${svgRef.viewBox.baseVal.y}, ${
+        svgRef.viewBox.baseVal.width - e.deltaY
+      },${svgRef.viewBox.baseVal.height - e.deltaY}`
+    );
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragPoint()) return;
+
+    const svgPoint = svgRef.createSVGPoint();
+    svgPoint.x = e.clientX;
+    svgPoint.y = e.clientY;
+    const mappedPoint = svgPoint.matrixTransform( dragPoint().screenCtm );
+
+    svgRef.setAttribute(
+      "viewBox",
+      `${dragPoint().x + (dragPoint().mappedPoint.x - mappedPoint.x)}, ${
+        dragPoint().y + (dragPoint().mappedPoint.y - mappedPoint.y)
+      }, ${dragPoint().width},${dragPoint().height}`
+    );
+  };
+
   return (
     <div class="MineSweeper300">
       <h1 class="title">MineSweeper300: PoC</h1>
@@ -14,40 +67,15 @@ const MineSweeper300 = () => {
         width="100%"
         height="100%"
         viewBox="-500, -500, 1000, 1000"
-        onmousedown={(e) =>
-          setDragPoint({
-            x: svgRef.viewBox.baseVal.x,
-            y: svgRef.viewBox.baseVal.y,
-            width: svgRef.viewBox.baseVal.width,
-            height: svgRef.viewBox.baseVal.height,
-            clientX: e.clientX,
-            clientY: e.clientY,
-          })
-        }
-        onwheel={(e) => {
-          svgRef.setAttribute(
-            "viewBox",
-            `${svgRef.viewBox.baseVal.x}, ${svgRef.viewBox.baseVal.y}, ${
-              svgRef.viewBox.baseVal.width - e.deltaY
-            },${svgRef.viewBox.baseVal.height - e.deltaY}`
-          );
-          console.log(e.deltaX, e.deltaY);
-        }}
-        onmouseup={(e) => setDragPoint(null)}
-        onmousemove={(e) =>
-          dragPoint() &&
-          svgRef.setAttribute(
-            "viewBox",
-            `${dragPoint().x + (dragPoint().clientX - e.clientX)}, ${
-              dragPoint().y + (dragPoint().clientY - e.clientY)
-            }, ${dragPoint().width},${dragPoint().height}`
-          )
-        }
-        onmouseleave={(e) => setDragPoint(null)}
+        onmousedown={handleMouseDown}
+        onwheel={handleWheel}
+        onmouseup={handleMouseUp}
+        onmousemove={handleMouseMove}
+        onmouseleave={handleMouseLeave}
       >
-        {new Array(20)
+        {new Array(30)
           .fill(null)
-          .map(() => new Array(20).fill(null))
+          .map(() => new Array(30).fill(null))
           .map((row, rowIdx) =>
             row.map((_, colIdx) => (
               <rect
