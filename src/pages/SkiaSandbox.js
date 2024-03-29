6715f13dc21a01a047e177a7098a31cf5cd32901
tkwk9 <tim.k.kwak@gmail.com>
2024-03-28 18:37:37 -0700

    add cooler shader

--- src/pages/SkiaSandbox.js
+++ src/pages/SkiaSandbox.js
@@ -4,15 +4,27 @@ import CanvasKitInit from "canvaskit-wasm";
 
 import "./SkiaSandbox.scss";
 
+// Source: @XorDev https://twitter.com/XorDev/status/1475524322785640455
 const shaderCode = `
+  uniform float2 iResolution;
   uniform float iTime;
 
-  half4 main(float2 coord) {
-    float3 white = float3(0,0,0);
-    float3 black = float3(1,0,0);
-    return half4(mix(white, black, sin(iTime)), 1);
+  vec4 main(vec2 FC) {
+    vec4 o = vec4(0);
+    vec2 p = vec2(0), c=p, u=FC.xy*2.-iResolution.xy;
+    float a;
+    for (float i=0; i<4e2; i++) {
+      a = i/2e2-1.;
+      p = cos(i*2.4+iTime+vec2(0,11))*sqrt(1.-a*a);
+      c = u/iResolution.y+vec2(p.x,a)/(p.y+2.);
+      o += (cos(i+vec4(0,2,4,0))+1.)/dot(c,c)*(1.-p.y)/3e4;
+    }
+    return o;
   }
 `;
+
+const canvasWidth = 800;
+const canvasHeight = 600;
 
 const SkiaSandbox = () => {
   CanvasKitInit({
@@ -31,11 +43,14 @@ const SkiaSandbox = () => {
       let currentTime = Date.now();
       let elapsedTime = currentTime - startTime;
       
+      // TODO: figure out if there's a way to pass named uniform
       paint.setShader(shaderFactory.makeShader([
-        // TODO: figure out if there's a way to pass named uniform
-        elapsedTime/1000.0 //iTime
+        canvasWidth, // iResolution.x
+        canvasHeight, // iResolution.y
+        elapsedTime/1000.0 // iTime
       ]));
-      canvas.drawRect(CanvasKit.LTRBRect(0, 0, 500, 400), paint);
+
+      canvas.drawRect(CanvasKit.LTRBRect(0, 0, canvasWidth, canvasHeight), paint);
       surface.flush();
 
       requestAnimationFrame(drawFrame);
@@ -47,11 +62,13 @@ const SkiaSandbox = () => {
     <div class="SkiaSandbox">
       <div
         class="title"
-        style={`display:flex;flex-direction:column;justify-content:center;align-items:center`}
+        style={`display:flex; flex-direction:column; justify-content:center; align-items:center`}
       >
         <h1 style={`padding: 0 0 20px`}>Skia Sandbox</h1>
       </div>
-      <canvas id="myCanvas" width="500" height="400"></canvas>
+      <div style={`background-color: black`}>
+        <canvas id="myCanvas" width={canvasWidth} height={canvasHeight}></canvas>
+      </div>
     </div>
   );
 };
